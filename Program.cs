06f4415b41e5a888e1ed1e3002bd4e0617191bff
CHANDRA BOSE <CHANDRA BOSE@LAPTOP-0D044MST>
2024-09-26 23:08:13 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace reversednumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int reversedNumber = ReverseNumber(number);
+            Console.WriteLine($"The reverse of {number} is {reversedNumber}.");
+        }
+
+        static int ReverseNumber(int num)
+        {
+            int reversed = 0;
+
+            while (num != 0)
+            {
+                int digit = num % 10; 
+                reversed = reversed * 10 + digit; 
+                num /= 10; 
+            }
+
+            return reversed;
+        }
+    }
+}
